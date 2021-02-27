6b0353cec237a4a960143e459dff0ad6ca7b2ec8
JonathanMary <jon85ma@gmail.com>
2021-02-27 02:37:10 +0100

    Cleaned CRA, basic structure, Favicon

---
+++ water-my-plants/src/components/EditProfile.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Authenticated user can update their phoneNumber and password.
+//Authenticated user can update their phoneNumber and password.
+
+
+const EditProfile = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EditProfile;
---
+++ water-my-plants/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//user can sign-up / create an account by providing a unique username, a valid mobile phoneNumber and a password.
+//user can login to an authenticated session using the credentials provided at account creation / signup.
+
+
+const Login = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Login;
