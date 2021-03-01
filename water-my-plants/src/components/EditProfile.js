abcf892758c1cd0357ffffd559c1b0601eacb5de
Chris Burton <chris@fullstackcaveman.com>
2021-03-01 16:10:26 -0700

    Styled EditProfileForm with styled components - added config.js to hold api constants

--- water-my-plants/src/components/EditProfile.js
+++ water-my-plants/src/components/EditProfile.js
@@ -1,18 +1,16 @@
-import React from "react";
-import EditProfileForm from "./EditProfileForm";
+import React from 'react';
+import EditProfileForm from './EditProfileForm';
 
 //Authenticated user can update their phoneNumber and password.
 //Authenticated user can update their phoneNumber and password.
-
 
 const EditProfile = () => {
-
-    return (
-        <div>
-            <h2>Edit Profile</h2>
-            <EditProfileForm />
-        </div>
-    )
-}
+	return (
+		<div>
+			{/* <h2>Edit Profile</h2> */}
+			<EditProfileForm />
+		</div>
+	);
+};
 
 export default EditProfile;
